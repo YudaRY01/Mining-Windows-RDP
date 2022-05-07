ade493f249fccbf634df94eacb35a7dbbf8002c4
4lT41R <105080763+4lT4lR@users.noreply.github.com>
2022-05-06 19:22:07 -0700

    Add files via upload

---
+++ CPUMiner.bat
@@ -0,0 +1,13 @@
+setx GPU_MAX_HEAP_SIZE 100	
+setx GPU_MAX_USE_SYNC_OBJECTS 1	
+setx GPU_SINGLE_ALLOC_PERCENT 100	
+setx GPU_MAX_ALLOC_PERCENT 100	
+setx GPU_MAX_SINGLE_ALLOC_PERCENT 100	
+setx GPU_ENABLE_LARGE_ALLOCATION 100
+setx GPU_MAX_WORKGROUP_SIZE 1024
+@echo off	
+cd %~dp0	
+	
+cls	
+cpuminer.exe -o stratum+tcp://yescrypt.eu.mine.zpool.ca:6233 -u DRjwyBu7dWkd7zJyhpVtgXC5LWbG51ooxP -p c=DGB
+pause
